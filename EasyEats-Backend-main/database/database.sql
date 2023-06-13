3e1e6d0bce43b0682b2993fd6c33697fc7cd63ae
unknown <memogarcia246@gmail.com>
2023-06-13 15:11:40 +0200

    database stuff

--- EasyEats-Backend-main/database/database.sql
+++ EasyEats-Backend-main/database/database.sql
@@ -1,3 +1,7 @@
+-- create a database called easy_eats
+CREATE DATABASE IF NOT EXISTS easy_eats;
+USE easy_eats;
+
 CREATE TABLE users(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE,
